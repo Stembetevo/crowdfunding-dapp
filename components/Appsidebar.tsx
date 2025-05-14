d44884c0ae79add12284f46b6755bd1cdd5a404d
Stembetevo <stephenkaruru05@gmail.com>
2025-05-15 00:06:38 +0300

    1.Navbar created👌👌

---
+++ components/Appsidebar.tsx
@@ -0,0 +1,67 @@
+import { CircleUserRound,  LayoutDashboard, LogOut, Plus } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+
+// Menu items.
+const items = [
+  {
+    name: 'Dashboard',
+    url: 'dashboard',
+    icon: LayoutDashboard,
+  },
+  {
+    name: 'Campaign',
+    url: '/create-campaign',
+    icon: Plus,
+  },
+  {
+    name: 'Profile',
+    url: '/profile',
+    icon: CircleUserRound,
+  },
+  {
+    name: 'Log out',
+    url: '/log-out',
+    icon: LogOut,
+  },
+  
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Crowdfunding App</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.name}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <a>
+                      <item.icon />
+                      <span>{item.name}</span>
+                    </a>
+                    </Link>
+                    
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
